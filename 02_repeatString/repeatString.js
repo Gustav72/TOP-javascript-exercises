116921dc8e181646e2c06f17c70af152f266f2ad
Wolfgang <wolfgangkirstein7@gmail.com>
2022-10-10 20:23:16 -0400

    Complete Exercises 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    
+    if (num >= 0) {
+        let repeat = ''
+        for(let i = 0; i < num; i++) {
+        repeat = repeat + string
+        }
+        return repeat;
+    }
+    else return 'ERROR'
 };
 
 // Do not edit below this line
