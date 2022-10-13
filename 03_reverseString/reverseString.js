a580b681be3d35f8cdf94f66f5b09a2eea333550
Wolfgang <wolfgangkirstein7@gmail.com>
2022-10-12 20:25:38 -0400

    Finish Exercise 3, Reverse String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    let reverse = [];
+    for(let i = 0; i < string.length; i++) {
+        reverse.unshift(string.charAt(i));
+    }
+    reverse = reverse.join('');
+    return reverse;
 
 };
 
