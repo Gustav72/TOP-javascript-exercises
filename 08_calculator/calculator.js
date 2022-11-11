8c92175ea3557e76ac862da04ffc5163d769bb2e
Wolfgang <wolfgangkirstein7@gmail.com>
2022-11-10 21:31:53 -0500

    Finish Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
+const add = function(x, y) {
+
+  if(x === undefined && y === undefined) return 0;
+  else if(x === undefined) return y;
+  else if(y === undefined) return x;
+  else return x + y;
 	
 };
 
-const subtract = function() {
+const subtract = function(x, y) {
+	
+  if(x === undefined && y === undefined) return 0;
+  else if(x === undefined) return -y;
+  else if(y === undefined) return x;
+  else return x - y;
 	
 };
 
-const sum = function() {
+const sum = function(x) {
+  let num = 0;
+  for(let i = 0; i < x.length; i++) {
+    num = num + x[i];
+  }
+	return num;
+};
+
+const multiply = function(x) {
+  let num = 1;
+  for(let i = 0; i < x.length; i++) {
+    num = num * x[i];
+  }
+	return num;
+};
+
+const power = function(x, y) {
+	
+  if(x === undefined && y === undefined) return 0;
+  else if(x === undefined) return 0;
+  else if(y === undefined) return 1;
+  else return Math.pow(x, y);
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(x) {
+	let num = 1;
+  for (let i = 1; i < x + 1; i++){
+    num = num * i;
+   }
+  return num;
 };
 
 // Do not edit below this line
