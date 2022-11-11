eb78c3bba6d3887ac4d55ceeb006ec5cc543edd3
Wolfgang <wolfgangkirstein7@gmail.com>
2022-11-11 11:24:21 -0500

    finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    
+    if(num < 0) return "OOPS";
+
+    let a = 0;
+    let b = 1;
+    let next;
+
+    for(let i = 0; i < num; i++)
+    {
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return a;
 
 };
 
