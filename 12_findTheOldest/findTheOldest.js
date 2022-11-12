3541e43ee2566fe7a0f3e4ae68f33980844e7ebb
Wolfgang <wolfgangkirstein7@gmail.com>
2022-11-11 20:19:04 -0500

    Pass 2 of 3 tests of findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let total = 0
+    let OldestPerson = {}
+    people.reduce((prev, curr) => {
+      if ((curr.yearOfDeath - curr.yearOfBirth) > total) {
+        total = (curr.yearOfDeath - curr.yearOfBirth);
+        OldestPerson = curr;
+      }
+      return prev;
+    })
+    return OldestPerson;
+
+  //  const res = people.reduce((prevVal,curVal) => {
+  //   return prevVal.yearOfDeath-prevVal.yearOfBirth < curVal.yearOfDeath-curVal.yearOfBirth ? curVal: prevVal;
+  // })
 };
 
 // Do not edit below this line
